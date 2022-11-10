71b1f888bd2fb97c12d6f92bcbc728fd87bf737c
Cedrick Williams <cedricktwilliams3@gmail.com>
2022-11-09 20:02:41 -0800

    web3 nft
    first commit

---
+++ Web 3.0/client/src/contract/index.js
@@ -0,0 +1,8 @@
+import contract from './AVAXGods.json';
+
+export const ADDRESS = '0xB01B5073D8357F3DA82dFafbCC8f882178061A05';
+//The ABI will allows the front-end side to call the functions that we have created inside of our smart contract
+export const { abi: ABI } = contract;
+//What is an ABI? 
+//An ABI is a Application Binary Interface in the context of computer science is an interface between two programs modules
+//often between operating systems and user programs.
