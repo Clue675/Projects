61d9a51a659777235f93e969e0f65547b518649d
Cedrick Williams <cedricktwilliams3@gmail.com>
2023-11-15 01:38:27 -0800

    Updating Git
    Added new projects

--- 3d_portfolio23/src/components/canvas/Computers.jsx
+++ 3d_portfolio23/src/components/canvas/Computers.jsx
@@ -1,94 +1,44 @@
-import { Suspense, useEffect, useState } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-// Computers component renders a 3D computer model
-const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+const Earth = () => {
+  const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <mesh>
-      {/* Add hemisphere light for soft ambient lighting */}
-      <hemisphereLight intensity={0.15} groundColor="black" />
-
-      {/* Wrap the spotlight in a group and apply position to the group */}
-      <group position={[-20, 50, 10]}>
-        {/* Add a spotlight for focused lighting */}
-        <spotLight
-          angle={0.12}
-          penumbra={1}
-          intensity={1}
-          castShadow
-          shadow-mapSize={1024}
-        />
-      </group>
-
-      {/* Add a point light for additional lighting */}
-      <pointLight intensity={1} />
-
-      {/* Add the computer model to the scene */}
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
 };
 
-// ComputersCanvas component wraps the Canvas component and handles responsiveness
-const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-
-    // Add the callback function as a listener for changes to the media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
-
+const EarthCanvas = () => {
   return (
     <Canvas
-      frameloop="demand"
       shadows
+      frameloop='demand'
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
-      {/* Show the CanvasLoader while the 3D model is being loaded */}
       <Suspense fallback={<CanvasLoader />}>
-        {/* Add OrbitControls to allow user interaction with the 3D scene */}
         <OrbitControls
+          autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
+        <Earth />
 
-        {/* Render the computer model */}
-        <Computers isMobile={isMobile} />
+        <Preload all />
       </Suspense>
-
-      {/* Preload all assets */}
-      <Preload all />
     </Canvas>
   );
 };
 
-export default ComputersCanvas;
+export default EarthCanvas;
